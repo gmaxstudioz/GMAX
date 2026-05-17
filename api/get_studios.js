e74e8e073f2ab9284f9bb71b0a6c19a2b7550ade
RHEM-01 <rhema.lemua@gmail.com>
2026-05-17 12:28:53 +0100

    refactor: remove deprecated database integration files

--- api/get_studios.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.studio.findMany().then(res => { console.log(JSON.stringify(res, null, 2)); prisma.$disconnect(); });
